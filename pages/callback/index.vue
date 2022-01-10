e7ce205b3faa719cb98c4d3572858e4aa6f845f8
koba <koba@growal.com>
2022-01-10 17:21:52 +0900

    lc

--- pages/callback/index.vue
+++ pages/callback/index.vue
@@ -1,3 +1,16 @@
 <template lang="pug">
 p Signingin...
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  mounted() {
+    // @ts-ignore
+    this.$auth0.setTokenByQuery()
+    // @ts-ignore
+    this.$router.replace(this.$PAGE_URL.TOP)
+  }
+})
+</script>
--- plugins/auth0.ts
+++ plugins/auth0.ts
@@ -1,4 +1,6 @@
 import Auth0Lock from 'auth0-lock'
+import jwtDecode from 'jwt-decode'
+import queryString from 'query-string'
 import nuxtConfig from '~/nuxt.config'
 
 const config = nuxtConfig.auth0
@@ -18,8 +20,43 @@ export class Auth0Util {
     lock.show()
   }
 
+  getQueryParams() {
+    return queryString.parse(location.hash)
+  }
+
+  setToken({
+    access_token,
+    id_token
+  }: {
+    access_token: string
+    id_token: string
+  }) {
+    const user = jwtDecode(id_token)
+    const { exp } = user as { exp: string }
+
+    const localStorage = window.localStorage
+    localStorage.setItem('accessToken', access_token)
+    localStorage.setItem('idToken', id_token)
+    localStorage.setItem('expiresAt', exp)
+    localStorage.setItem('user', JSON.stringify(user))
+  }
+
+  setTokenByQuery() {
+    this.setToken(
+      this.getQueryParams() as {
+        access_token: string
+        id_token: string
+      }
+    )
+  }
+
   getBaseUrl() {
     return `${window.location.protocol}//${window.location.host}`
+  }
+
+  isAuthenticated() {
+    const expiresAt = Number(window.localStorage.getItem('expiresAt'))
+    return new Date().getTime() / 1000 < expiresAt
   }
 }
 
